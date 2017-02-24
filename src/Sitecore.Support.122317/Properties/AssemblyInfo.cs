4c0ac9e5e5b4b403eb01647a40a0fcabdd781135
Pushpraj Ruhal <pur@sitecore.net>
2017-02-24 08:23:10 +0800

    The module 'XFrameOptionsHeaderModule' mustn't pick up front-end requests.

---
+++ src/Sitecore.Support.122317/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.122317")]
+[assembly: AssemblyProduct("Sitecore.Support.122317")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Fix: The module 'XFrameOptionsHeaderModule' mustn't pick up front-end requests.")]
+[assembly: AssemblyCompany("Sitecore")]
+
---
+++ src/Sitecore.Support.122317/Web/XFrameOptionsHeaderModule.cs
@@ -0,0 +1,37 @@
+﻿using Sitecore.Diagnostics;
+using System;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Web
+{
+  internal class XFrameOptionsHeaderModule : IHttpModule
+  {
+    protected HttpApplication Application;
+    private const string X_Frame_Options = "X-Frame-Options";
+
+    private void BeginRequestHandler(object sender, EventArgs e)
+    {
+      Assert.ArgumentNotNull(sender, "sender");
+      Assert.ArgumentNotNull(e, "e");
+      if (HttpContext.Current.Request.RawUrl.StartsWith("/sitecore/shell"))
+      {
+        HttpResponse response = HttpContext.Current.Response;
+        if (response.Headers.AllKeys.All<string>(key => (key != X_Frame_Options)) && (response.ContentType == "text/html"))
+        {
+          response.Headers.Add(X_Frame_Options, "SAMEORIGIN");
+        }
+      }
+    }
+
+    public virtual void Init(HttpApplication context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      this.Application = context;
+      this.Application.BeginRequest += new EventHandler(this.BeginRequestHandler);
+    }
+    public virtual void Dispose()
+    {
+    }
+  }
+}
